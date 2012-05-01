65b33a8c7e79f42bf25e1beb015ad501d0df7bd1
davboecki <dboeken@yahoo.de>
2012-05-01 03:05:20 +0200

    Added 4096 IDs fix

--- src/main/java/de/davboecki/multimodworld/plugin/listener/PacketListener.java
+++ src/main/java/de/davboecki/multimodworld/plugin/listener/PacketListener.java
@@ -1,18 +1,20 @@
 package de.davboecki.multimodworld.plugin.listener;
 
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.logging.Logger;
+
 import org.bukkit.Bukkit;
 import org.bukkit.craftbukkit.entity.CraftPlayer;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
 import org.bukkit.entity.Player;
 
-import net.minecraft.server.Packet;
-import net.minecraft.server.Packet5EntityEquipment;
+import net.minecraft.server.*;
 import de.davboecki.multimodworld.plugin.PrivatChest;
 import de.davboecki.multimodworld.api.plugin.PlayerStatus;
 
 public class PacketListener{
 	
+	Logger logger = Logger.getLogger("Bukkit");
 	PrivatChest plugin;
 	public boolean AllowModLoaderPacket = false;
 	
@@ -26,7 +28,54 @@ public class PacketListener{
 		try {
 			if(packet == null) return true;
 			if(player == null) return true;
-			if(packet instanceof Packet5EntityEquipment){
+			/*
+			 if(log && !(packet instanceof Packet50PreChunk)) {
+				StringBuilder logentry = new StringBuilder();
+				logentry.append("Player:"+player.getName()+", Packet:"+packet.getClass().getName());
+				logentry.append(", ");
+				try {
+					Field a = Packet.class.getDeclaredField("a");
+					a.setAccessible(true);
+					HashMap<Class<?>, Integer> aMap = (HashMap<Class<?>, Integer>)a.get(null);
+					logentry.append("Real Number: "+aMap.get(packet.getClass())+", ");
+				} catch(Exception e) {}
+				for(Field f:packet.getClass().getDeclaredFields()) {
+					try {
+						try {
+							f.setAccessible(true);
+						} catch(Exception e) {}
+						Object o = f.get(packet);
+						try {
+							Object[] oarray = (Object[])o;
+							StringBuilder arrayentry = new StringBuilder();
+							arrayentry.append("[");
+							for(Object opart:oarray) {
+								if(arrayentry.toString().equals("[")) {
+									arrayentry.append(opart.toString());
+								} else {
+									arrayentry.append(", "+opart.toString());
+								}
+							logentry.append(f.getName()+": '"+arrayentry+"]', ");
+							}
+						} catch(Exception e) {
+							logentry.append(f.getName()+": '"+o+"', ");
+						}
+					} catch(Exception e) {
+						logentry.append(f.getName()+": 'ACCESS_DENIED', ");
+					}
+				}
+				logger.info(logentry.toString());
+			}
+			*/
+			if(packet instanceof Packet53BlockChange) {
+				try {
+					packet.getClass().getDeclaredField("vanilla"); //Crash and go on if wrong version
+					Packet53BlockChange packet53 = (Packet53BlockChange)packet;
+					packet53.vanilla = PlayerStatus.isVanilla(player.getName());
+				} catch(Exception e) {
+					
+				}
+			} else if(packet instanceof Packet5EntityEquipment) {
 				Packet5EntityEquipment Equipment = (Packet5EntityEquipment)packet;
 				if(PlayerStatus.isVanilla(player.getName())) {
 					if(plugin.MultiModWorld.isIdAllowed(player.getWorld().getName(), Equipment.c)) {
